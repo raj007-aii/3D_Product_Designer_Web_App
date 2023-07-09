d4c7c6c2052390de76ad512f31ebaeac70c2ecf5
raj007-aii <rajbhuva25103@gmail.com>
2023-07-09 18:26:32 +0530

    hii

---
+++ client/src/components/AIPicker.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import CustomButton from './Custombutton'
+
+const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
+  return (
+    <div className='aipicker-container'>
+      <textarea 
+        placeholder='Ask Something...'
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        className='aipicker-textarea'
+        />
+      
+      <div className='flex flex-wrap gap-3'>
+        {generatingImg ? (
+          <CustomButton
+            type='outline'
+            title='Asking AI...'
+            customStyles='text-xs'
+          />
+        ) : (
+          <>
+          <CustomButton
+            type='outline'
+            title='AI Logo'
+            handleClick={() => handleSubmit('logo')}
+            customStyles='text-xs'
+          />
+
+          <CustomButton
+            type='filled'
+            title='AI Full'
+            handleClick={() => handleSubmit('logo')}
+            customStyles='text-xs'
+          />
+
+          </>
+        )}
+
+      </div>
+    </div>
+  )
+}
+
+export default AIPicker
---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import Custombutton from "./Custombutton";
+import FilePicker from "./FilePicker";
+import ColorPicker from "./ColorPicker";
+import AIPicker from "./AIPicker";
+import Tab from "./Tab";
+
+export { Custombutton, FilePicker, ColorPicker, AIPicker, Tab }
